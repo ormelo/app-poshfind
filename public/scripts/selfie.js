1c36a1a530816fd2f67d47bcf04ab143477100f5
Sampath Kumar N <sampathkumar.nagarajan@in.tesco.com>
2017-08-31 10:59:31 +0530

    Day 6

--- public/scripts/selfie.js
+++ public/scripts/selfie.js
@@ -42,7 +42,15 @@ function getStream() {
 
 function gotStream(stream) {
   window.stream = stream; // make stream available to console
-  videoElement.srcObject = stream;
+  // videoElement.srcObject = stream;
+  videoElement.src = window.URL.createObjectURL(stream);
+      streamTrack = stream.getTracks()[0];
+      // Play the video element to start the stream.
+      videoElement.play();
+      videoElement.onplay = function() {
+        showVideo();
+      };
+
 }
 
 function handleError(error) {
