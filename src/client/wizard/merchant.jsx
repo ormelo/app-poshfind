5cae40889d1cf9838f5a659dccb59239e3eaa9e1
sampathoops <sampath.oops@gmail.com>
2017-10-10 15:58:12 +0530

    Day 24

--- src/client/wizard/merchant.jsx
+++ src/client/wizard/merchant.jsx
@@ -24,10 +24,10 @@ class Merchant extends Component {
             <div style={{fontSize:'22px',textAlign:'center',lineHeight:'28px',paddingTop:'6px'}}><img style={{width:'120px'}} src="../img/merch_logo.png"/> 
             </div>
             <hr style={{marginTop: '10px', marginBottom: '14px'}}/>
-            <div style={{width: '360px', margin: '0 auto', left: '0',right: '0'}}>
+            <div style={{width: '250px', margin: '0 auto', left: '0',right: '0'}}>
             <span className="rating"><b style={{width: '3.6em'}}><i>Rating: 4.2 out of 5</i></b></span><span style={{fontSize: '12px'}}>(22)</span>
             <div className="description" style={{paddingBottom:'0px',marginTop:'0px',display:'inline',marginLeft: '12px',fontSize: '16px'}}>
-                Select Size:&nbsp;&nbsp; <select id="sizes" style={{marginTop:'5px',height:'40px',fontSize:'18px'}}>
+                Size:&nbsp;&nbsp; <select id="sizes" style={{marginTop:'5px',height:'40px',fontSize:'18px'}}>
                     <option id="s1" value="m">M</option>
                     <option id="s2" value="l">L</option>
                     <option id="s3" value="xl">XL</option>
@@ -35,11 +35,11 @@ class Merchant extends Component {
                 </select>
             </div>
             </div>
-            <div style={{margin: '0 auto',left:'0',right:'0'}}>
-            ₹<span id="buyPrice"></span>
+            <div style={{margin: '0 auto',left:'0',right:'0',fontSize:'16px',color:'#ff9800'}}>
+            ₹<span id="buyPrice" style={{fontWeight:'bold'}}></span>
             </div>
             <br/>
-            <div><img id="currProduct" style={{width:'140px'}} src=""/></div>
+            <div><img id="currProduct" style={{width:'100px'}} src=""/></div>
             <input type="text" placeholder="Enter Pincode for shipping" style={{width:'280px',height:'40px',padding:'10px',fontSize:'16px',marginTop:'12px',border:'1px solid #d2d2d2'}}/>
             <div className="btn-check" id="checkAvailability"><span>Check Availability</span></div>
           </div>
