98b59921840a298046088d5cf2d27aad20be7c02
sampathoops <sampath.oops@gmail.com>
2017-11-25 15:23:45 +0530

    Day 45

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/fit-profile', function(request, response) {
   response.sendFile(path.resolve(__dirname, 'public', 'steps.html'));
 });
 
+app.get('/home', function(request, response) {
+  response.sendFile(path.resolve(__dirname, 'public', 'home.html'));
+});
+
 app.get('/fit-test', function(request, response) {
   response.sendFile(path.resolve(__dirname, 'public', 'steps.html'));
 });
