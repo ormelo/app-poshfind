f2acc32ed15577355dba99720b6d1a61580312c0
sampathoops <sampath.oops@gmail.com>
2017-12-19 19:38:57 +0530

    Day 51

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@
 var doCache = true;
 
 // Name our cache
-var CACHE_NAME = 'poshfind-cache-v1';
+var CACHE_NAME = 'attirist-cache-v1';
 
 // Delete old caches that are not our current one!
 self.addEventListener("activate", event => {
